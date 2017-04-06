65a5fe0a7ca8bb868a48838bafd094dd868a5e95
Alexander Efimov <alefimov@griddynamics.com>
2017-04-06 17:41:43 +0300

    Fixed ssh rc

--- roles/common/files/ssh.rc.sh
+++ roles/common/files/ssh.rc.sh
@@ -5,15 +5,3 @@ if [[ -n "$SSH_TTY" ]] && [[ -S "$SSH_AUTH_SOCK" ]]; then
     ln -sf "$SSH_AUTH_SOCK" "${HOME}/.ssh/ssh_auth_sock"
     export SSH_AUTH_SOCK="${HOME}/.ssh/ssh_auth_sock"
 fi
-
-# Taken from the sshd(8) manpage.
-if read proto cookie && [ -n "$DISPLAY" ]; then
-        if [ `echo $DISPLAY | cut -c1-10` = 'localhost:' ]; then
-                # X11UseLocalhost=yes
-                echo add unix:`echo $DISPLAY |
-                    cut -c11-` $proto $cookie
-        else
-                # X11UseLocalhost=no
-                echo add $DISPLAY $proto $cookie
-        fi | xauth -q -
-fi
