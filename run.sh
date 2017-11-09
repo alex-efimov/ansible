3c91af83c142f5c56b69099df46098f726cfdd12
Alex Efimov <efimov.ay.work@gmail.com>
2017-11-09 15:39:18 -0500

    Added MST

--- run.sh
+++ run.sh
@@ -4,8 +4,11 @@ GROUP=""
 
 [[ "$1" ]] && \
   GROUP="-l $1"
+[[ "$2" ]] && \
+  TAGS="--tags '$2'"
 
 ansible-playbook -vvv \
+  -e 'host_key_checking=False' \
   --ask-vault-pass\
-  -i ./inventory/ ${GROUP}\
+  -i ./inventory/ ${GROUP} ${TAGS} \
   ./site.yml
